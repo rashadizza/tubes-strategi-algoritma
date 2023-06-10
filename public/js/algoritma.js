cf7674449b03d2c59435df448fcc19e2a48676ae
rashadizza <54693288+rashadizza@users.noreply.github.com>
2023-06-10 16:29:08 +0700

    update dikit

--- public/js/algoritma.js
+++ public/js/algoritma.js
@@ -539,3 +539,13 @@ function resetSemua() {
 //     notifMisiHTML += "</pre>";
 //     notifMisiElement.innerHTML = notifMisiHTML;
 // }
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
